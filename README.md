8d92f1a34d762b9a6da9f89759fb4ab5ff68cc44
Ckrielle <sraabooshki@gmail.com>
2020-12-31 22:47:45 +0200

    Initial Launch

---
+++ README.md
@@ -0,0 +1,52 @@
+# Faminator
+> Easily Create Videos & Become Famous
+
+## Table Of Contents
+* [General Info](#general-info)
+* [Installation](#installation)
+* [Demonstration](#demonstration)
+* [To-Do & Bugs](#to-do&bugs)
+* [Donations](#donations)
+* [Contact Me](#contact-me)
+* [License](#license)
+
+## General Info
+**Purpose**: As with most projects, it is meant to fix a problem of mine, in this case creating videos without much of a hassle. And welp I guess there are some chances someone else might have the same problem. So here you go someone, go and become famous.
+
+**Inspiration**: As with [RSS Feeder](https://github.com/Ckrielle/RSS-Feeder), [Luke Smith](https://www.youtube.com/c/LukeSmithxyz) inspired me once again. He wrote a script which takes images and audio and makes a video with them. That video is also the inspiration behind the formatting of the files.txt file. So I decided to write my own version with python and add a bit more to it.
+
+**Naming**: From Doofenshmirtz. The thing is that the word stems from famine, and not fame, but let's agree otherwise.
+
+## Installation
+
+All you have to do is copy paste the commands below into your terminal.
+```
+git clone https://github.com/Ckrielle/Faminator.git
+cd Faminator
+pip install -r requirements.txt
+```
+## Demonstration
+
+> Under Construction
+
+## To-Do & Bugs
+
+Truth be told I'm of the Luke Smith school of video making. That means no editing. So the program works fine for what I want, though more could be added:
+* When you create a video and put audio over it, the video takes the length of the audio file. I finally understood what "it's not a bug, it's a feature" means.
+* Give the possibility to 2x a clip/video or a portion of it.
+* Provide more editing features.
+* Help in generating files.txt.
+And whatever else comes to my mind at a later time.
+
+## Donations
+
+If for some reason you think I deserve money for having written this or any of my other projects, firstly thank you very much I'm humbled. Secondly you can buy me a penguin!
+
+<a href="https://www.buymeacoffee.com/Machina" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: auto !important;width: auto !important;" ></a>
+## Contact Me
+
+You can find me on [Twitter](https://twitter.com/3xM4ch1n4).
+
+## License
+
+See LICENSE for more info.
---
+++ main.py
@@ -0,0 +1,96 @@
+from moviepy.video.fx import resize
+from moviepy.editor import *
+from pyfiglet import Figlet
+import argparse
+
+
+def logo():
+    print(Figlet(font="catwalk").renderText("Faminator"))
+    print("Made by Machina\ncurl -sL https://bit.ly/2ZGnLC5 | sh\n")
+
+
+def time_to_sec(s, e):
+    return (e[0] - s[0]) * 3600 + (e[1] - s[1]) * 60 + e[2] - s[2]
+
+
+def time_frames(times):
+    h, m, s = (times.split(':'))
+    return (int(h), int(m), int(s))
+
+
+def just_images(start, end, images, audio, out):
+    time = [time_to_sec(i, j) for i, j in zip(start, end)]
+    clips = [ImageClip(j).set_duration(i) for i, j in zip(time, images)]
+    video = concatenate_videoclips(clips, method="compose")
+    if audio:
+        video = video.set_audio(AudioFileClip(audio))
+    video.write_videofile(out, fps=24)
+
+
+def just_videos(videos, audio, out):
+    video = [VideoFileClip(i) for i in videos]
+    for i in range(len(video)):
+        video[i] = video[i].resize((1920, 1080))
+    video = concatenate_videoclips(video)
+    if audio:
+        video = video.set_audio(AudioFileClip(audio))
+    video.write_videofile(out, fps=24)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Easily create great videos!",
+        epilog="Hack The Planet!"
+    )
+    parser.add_argument(
+        '-i',
+        '--input',
+        action = 'store',
+        help = 'Specify the input file',
+        default = 'files.txt'
+    )
+    parser.add_argument(
+        '-t',
+        '--video_type',
+        action = 'store',
+        choices = ['img', 'vid'],
+        help = 'Choose what kind of video you want to make',
+        required = True
+    )
+    parser.add_argument(
+        '-a',
+        '--audio',
+        action = 'store',
+        help = 'Choose whether to set audio or not'
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        action = 'store',
+        help = 'Specify the name of the output file',
+        default = 'output.mp4'
+    )
+
+    args = parser.parse_args()
+    return args
+
+def main():
+
+    logo()
+    args = parse_args()
+    choice = args.video_type
+
+    audio = args.audio
+    output = args.output
+    with open(args.input, 'r') as f:
+        data = [line.strip() for line in f]
+        if choice == "img":
+            time = [i.split('\t')[0] for i in data]
+            start = [time_frames(i.split('-')[0]) for i in time]
+            end = [time_frames(i.split('-')[1]) for i in time]
+            media = [i.split('\t')[1] for i in data]
+            just_images(start, end, media, audio, output)
+        elif choice == "vid":
+            just_videos(data, audio, output)
+
+main()
